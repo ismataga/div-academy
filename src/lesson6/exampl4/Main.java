3770a00ea61e6c5c6f1c72294bea29fce3f7557d
Ismat <ismataga1@gmail.com>
2023-04-06 02:41:58 +0400

    Student registration system

--- src/lesson6/exampl4/Main.java
+++ src/lesson6/exampl4/Main.java
@@ -1,38 +1,46 @@
-//package lesson6.exampl4;
-//
-//import lesson6.exampl4.global.Global;
-//
-//import java.util.Scanner;
-//
-//import static lesson6.exampl4.global.Global.students;
-//
-//
-//public class Main {
-//    public static void main(String[] args) {
-////        StudentServices.register();
-////        StudentServices.show();
-//        findStudent(students, "ali");
-//
-//
-//
-//
-//    }
-//    public static void findStudent(Student[] students, String search) {
-////        Scanner sc = new Scanner(System.in);
-////        System.out.print("Enter users: ");
-////        String search = sc.nextLine();
-////        boolean found = false;
-//
-//
-//        for (int i = 0; i < students.length; i++) {
-//
-//            if (students.name.contains(search) || students.surname.contains(search)) {
-//                System.out.println(students[i] + "[" + i + "]");
-//
-//            }
-//        }
-//
-//
-//    }
-//}
-//
+package lesson6.exampl4;
+
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        StudentServices service = new StudentServices();
+        Scanner input = new Scanner(System.in);
+
+        studentRegistration(service, input);
+
+
+    }
+
+    private static void studentRegistration(StudentServices service, Scanner input) {
+        while (true) {
+
+            printCommand();
+
+            int add = input.nextInt();
+
+            switch (add) {
+                case 0 -> {
+                    return;
+                }
+                case 1 -> service.register();
+                case 2 -> service.show();
+                case 3 -> service.findStudent();
+                default -> System.out.println("Invalid option");
+            }
+        }
+    }
+
+    public static void printCommand() {
+        System.out.println("0. Exit system\n" +
+                "1. Register student\n" +
+                "2. Show student\n" +
+                "3. Search student\n"
+        );
+    }
+
+}
+
--- src/lesson6/exampl4/global/Global.java
+++
@@ -1,7 +0,0 @@
-package lesson6.exampl4.global;
-
-import lesson6.exampl4.Student;
-
-public class Global {
-    public static Student[] students = null;
-}
