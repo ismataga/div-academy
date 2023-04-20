5ee8fd2fae5f11c9f65d01ad6b6c50eac3305759
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-04-20 04:33:39 +0400

    Add  new tasks

--- src/lesson9/crud/demo/Person.java
+++ src/lesson9/crud/demo/Person.java
@@ -22,9 +22,9 @@ public class Person {
         return id;
     }
 
-//    public void setId(long id) {
-//        this.id = id;
-//    }
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
